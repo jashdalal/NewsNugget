a20fa7727cc9799c70d704d58c9449f36c4db1dd
Jash Dalal <dalal.jash@gmail.com>
2024-04-07 18:51:05 -0400

    Fetch latest news with hardcoded query and GET newsdata API

---
+++ api_news/fetch_news.py
@@ -0,0 +1,62 @@
+import json
+import requests
+
+
+def fetch_news(api_key, query, language='en'):
+    url = f'https://newsdata.io/api/1/news'
+    params = {
+        'apikey': api_key,
+        'q': query,
+        'language': language
+    }
+
+    try:
+        response = requests.get(url, params=params)
+        response.raise_for_status()  # Raise an exception for 4XX or 5XX errors
+
+        print(f"Request URL: {response.request.url}")
+
+        # Parse the JSON response
+        data = response.json()
+
+        # Parse the JSON response
+        data = response.json()
+        return data
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching news: {e}")
+        return None
+
+def main():
+    # Set your NewsData.io API key here
+    api_key = 'pub_4128925dfa2a70c562b279e6cda7553c93e46'
+
+    # Set query parameters
+    query = 'bollywood'
+
+    language = 'en'
+
+    # Fetch news articles
+    news_data = fetch_news(api_key, query, language)
+
+    if news_data:
+        if 'results' in news_data:
+            found_paid_content = False
+
+            for article in news_data['results']:
+                if 'content' in article and article['content'] == 'ONLY AVAILABLE IN PAID PLANS':
+                    found_paid_content = True
+                    break
+
+            if found_paid_content:
+                with open('sample_data.json', 'r', encoding='utf-8') as f:
+                    news_data = json.load(f)
+       
+        # Pretty print the response
+        print(json.dumps(news_data, indent=4))
+
+    else:
+        print("Failed to fetch news data.")
+
+if __name__ == "__main__":
+    main()
